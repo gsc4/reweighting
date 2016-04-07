86ecdf1ff724069f0c9e1b696e505b7a1c54e20a
Greg Campo <gsc4@rice.edu>
2016-04-07 17:00:32 -0500

    melting curve blessed by the glorious light of ~modular programming~

---
+++ mc_refactored.py
@@ -0,0 +1,17 @@
+import numpy as np
+import os
+import pymbar
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+# I'm a badass and I don't follow the rules
+from  mbar_setup import *
+
+if __name__ == "__main__":
+
+    print 'reading temps'
+
+    temps = read_filenames( 'short_temps' , '../../scratch/SH3.constant.temp/test_melt/0_initial_data/')
+
+    
+
