88c4a675ca375796396b25b56c2562023569d974
Greg Campo <gsc4@rice.edu>
2016-05-31 11:13:28 -0500

    added check to umbrella code to make sure num_interp was int, updated docstrings to comply with format guidelines

--- mbar_setup.py
+++ mbar_setup.py
@@ -14,23 +14,26 @@ kb_KJ_mol = .0083145
 
 def read_filenames(names_file, path): 
     """ 
-    Reads names_file to make a list of names
+    Reads file to make a list of names
 
-    Keyword arguments:
-    names_file -- (string) name of file containing beginnings of other filenames
+    Parameters
+    ----------
+    names_file : string
+        Name of file
+    path : string
+        Path from to file location
 
-    Returns:
-    names -- (list) contains the filename beginnings for the data to be read in later
+    Returns
+    -------
+    names : list of strings
+        Identifying name parts of files to be read in later
     """
-
-    # Get to the right place
+    
     cwd = os.getcwd()
     os.chdir(path)
-
-    # The file storing names has to be in a very specific format for this to work (newline after every name)
-    names  = [ x.strip("\n") for x in open(str(names_file), "r").readlines() ]  
     
-    # Take me home
+    names = [ x.strip("\n") for x in open(str(names_file), "r").readlines() ]  
+    
     os.chdir(cwd)    
 
     return names
@@ -39,20 +42,33 @@ def read_data(name_start, name_end, path, stride=1):
     """
     Reads the data found in the named files on the given path
 
-    Keyword arguments:
-    names_start -- (list) start of names of files to read
-    name_end -- (string) end of names of files to read (file type included)
-    path -- (string) path to files to be read
-    stride (optional) -- (int) only take  data every stride distance
+    Parameters
+    ----------
+    names_start : list of strings
+        Identifying name parts (the beginning) of files to read
+    name_end : string
+        End of files to read including filetype (common to all files)
+    path : string
+        Path to file location
+    stride : int, optional
+        Only consider data every stride distance
 
-    Returnss:
-    data -- (numpy.ndarray) array of read in data
-    data_long -- (numpy.ndarray) 1xn array of all the data read in (a vector)
+    Returns
+    -------
+    data : numpy.ndarray
+        Array of read-in data
+    data_long : numpy.ndarray
+        1xn array of all the read-in data (a vector)
     """ 
 
     names_strided = name_start[::stride]
-
-    data = np.array([ np.loadtxt( str(path) + ("{}{}").format(x, name_end), usecols=[1]) for x in names_strided ] )
+    
+    if name_end == '/Q.dat':
+        data = np.array([np.loadtxt( str(path) + ("{}{}").format(x, name_end))\
+             for x in names_strided ])
+    else:
+        data = np.array([np.loadtxt( str(path) + ("{}{}").format(x, name_end),
+             usecols=[1]) for x in names_strided ] )
     
     data_long = np.concatenate(data)
 
@@ -60,19 +76,30 @@ def read_data(name_start, name_end, path, stride=1):
 
 def temp_series_u_kn_N_k(temps, energies, Etot, num_interp = 2):
     """
-    Creates u_kn and N_k, interpolates 2 points by default        
+    Creates inputs for mbar on constant temperature simulations
 
-    Keyword arguments:
-    temps -- (list of strings) temperatures at which data was taken
-    energies -- (numpy.ndarray)  kxn array of energy data
-    Etot -- (numpy.ndarray) 1x(k*n) array of energy data (vector)
-    num_interp (optional) -- (int) number interpolating points
+    Parameters
+    ----------
+    temps : list of strings
+        Temperatures at which data was taken
+    energies : numpy.ndarray
+        Array of energy data
+        Dimensions: (num thermo states) x (num frames)     
+    Etot : numpy.ndarray
+        Vector of energy data
+        Dimensions: 1 x ((num thermo states)*(num frames)) 
+    num_interp : int, optional
+        Number interpolating points
 
-    Returns:
-    u_kn -- (numpy.ndarray) every frame evaluated at every thermodynamic state
-    N_k -- (numpy.ndarray) number of actual data frames for corresponding T
-    T -- (numpy.ndarray) all temperatures (including interpolated ones)
-
+    Returns
+    -------
+    u_kn : numpy.ndarray
+        Every frame evaluated at every thermodynamic state
+        Dimensions: (num thermo states) x ((num thermo states)*(num frames))
+    N_k : numpy.ndarray
+        Number of data frames for each temperature in `T`
+    T : numpy.ndarray
+        All temperatures (including interpolated ones)
     """
     
     flt_temps = [ float(x) for x in temps ]
@@ -98,23 +125,27 @@ def temp_series_u_kn_N_k(temps, energies, Etot, num_interp = 2):
     return u_kn, N_k, T
 
 def calc_Cv(mbar, Etot, T):
-    
     """
-    Calculates Cv at a temperature close to the the temps where the data for Etot was taken
+    Calculates heat capacity for given temperatures
 
-    Inputs:
+    Parameters
+    ----------
+    mbar : mbar object
+        Needed for calcualtions using pymbar      
+    Etot : numpy.ndarray
+        Vector of energy data
+        Dimensions: 1 x ((num thermo states)*(num frames)) 
+    T : numpy.ndarray
+        All temperatures (including interpolated ones)
     
-        mbar - (mbar object) need this for mbar stuff to work
-
-        Etot - (numpy.ndarray) Energies from all trials concatenated together
-
-    Outputs:
-
-        Cv - (numpy.ndarray) array containing Cv as a function of T
-
+    Returns
+    -------
+    Cv : numpy.ndarray
+        Array of heat capacity as a function of T
+        Dimensions: 1 x (num temps in `T`)
     """
 
-    B = 1/(kb_KJ_mol*T)    
+    beta = 1/(kb_KJ_mol*T)    
 
     # Calculate expectations for E
     (Eavg, dEavg) = mbar.computeExpectations(Etot)
@@ -123,12 +154,52 @@ def calc_Cv(mbar, Etot, T):
     (E2avg, dE2avg) = mbar.computeExpectations(Etot**2)
 
     # Cv = (1/kb*T^2)*( <E^2> - <E>^2)
-    Cv = kb_KJ_mol*(B**2)*(E2avg - Eavg**2)
+    Cv = kb_KJ_mol*(beta**2)*(E2avg - Eavg**2)
 
     return Cv
 
 def umbrella_u_kn_N_k(r0, energies, T, atom_pair, path, num_interp=2):
-    
+    """
+    Creates inputs for mbar on umbrella sampled simulations
+
+    Parameters
+    ----------
+    r0 : list of strings
+        Umbrella centers for which data was taken            
+    energies : numpy.ndarray
+        Array of energy data
+        Dimensions: (num thermo states) x (num frames)
+    atom_pair : np.ndarray   
+        Numbers of the 2 atoms that were pulled by umbrella potential
+        Dimensions: 1 x 2
+    num_interp : int, optional
+        Number of interpolating points
+
+    Returns
+    -------
+    u_kn : numpy.ndarray
+        Every frame evaluated at every thermodynamic state
+        Dimensions: (num thermo states) x ((num thermo states)*(num frames))
+    N_k : numpy.ndarray
+        Number of data frames for each temperature in `r0_interp`
+    r0_interp : numpy.ndarray
+        All umbrella centers (including interpolated ones)
+    r1N_all : numpy.ndarray
+        Vector of all pairwise distances for the 2 atoms for all thermo states
+        Dimensions: 1 x ((num thermo states)*(num frames))
+ 
+    """
+
+    # Check if num_interp is a number
+    if not isinstance(num_interp, int): 
+        try:
+            print "Rouding interpolations to nearest non-negative integer"
+            num_interp = int(round(float(num_interp)))
+            if num_interp < 0:
+                num_interp = 0
+        except TypeError:
+            raise Exception("num_interp has to be a number (preferably int)")
+
     cwd = os.getcwd()
       
     E_unbias = []
@@ -155,7 +226,7 @@ def umbrella_u_kn_N_k(r0, energies, T, atom_pair, path, num_interp=2):
         # Hop into directory w/ data    
         os.chdir(r0[i])
         
-        ###### NEED TO READ THIS VALUE FROM SOMEWHERE ###### 
+        ###### NEED TO READ THIS VALUE FROM SOMEWHERE ######
         k_umb = 2   
         
         # Read end-end distances from file, if file doesn't exist, make one
@@ -179,7 +250,7 @@ def umbrella_u_kn_N_k(r0, energies, T, atom_pair, path, num_interp=2):
 
     beta = 1./(kb_KJ_mol*T)
 
-    # Initialize array for u_kn and value for indexing
+    # Initialize array for u_kn
     u_kn = np.zeros((len(r0_interp) + 1 ,len(E_unbias)), float)
 
     for k in range(len(r0_interp)):
@@ -196,35 +267,50 @@ def umbrella_u_kn_N_k(r0, energies, T, atom_pair, path, num_interp=2):
     # Hop back out to parent directory so we can do it again
     os.chdir(cwd)
         
-    return u_kn, N_k, r0_interp 
+    return u_kn, N_k, r0_interp, r1N_all 
 
-def free_eng_profile(mbar, Q_long,  numbins=100):
+def free_eng_profile(mbar, rxn_coord, numbins=100):
+    """
+    Calculates free energy as a function of a reaction coordinate
 
-    bin_n, bin_edges = np.histogram(Q_long, bins=numbins)
-    bin_p = bin_n/len(Q_long)
+    Parameters
+    ----------
+    mbar : mbar object
+        Needed for calculations using pymbar
+    rxn_coord :
+        Vector of reaction coordinate data for all frames in all thermo states
+    numbins : int, optional
+        Number of bins used in the indicator function `h`
+
+    """
+
+    bin_n, bin_edges = np.histogram(rxn_coord, bins=numbins)
     bin_mid = .5*(bin_edges[1:] + bin_edges[:-1])
 
-    p_of_Q = np.zeros( (len(r0_interp), numbins), float)
-    p_of_Q_min_0 =  np.zeros( (len(r0_interp), numbins), float)
-    dp_of_Q = np.zeros( (len(r0_interp), numbins), float)
+    p_of_rxn_coord = np.zeros( (len(r0_interp) + 1, numbins), float)
+    p_of_rxn_coord_min_0 =  np.zeros( (len(r0_interp) + 1, numbins), float)
+    dp_of_rxn_coord = np.zeros( (len(r0_interp) + 1, numbins), float)
 
     for i in range(numbins):
-        h = (Q_long > bin_edges[i]) & (Q_long < bin_edges[i+1])
-        p_of_Q[:,i], dp_of_Q[:,i] = mbar.computeExpectations(h.astype(int))
-        p_of_Q_min_0[:,i] = p_of_Q[:,i] - min(p_of_Q[:,i])
+        h = (rxn_coord > bin_edges[i]) & (rxn_coord < bin_edges[i+1])
+        p_of_rxn_coord[:,i], dp_of_rxn_coord[:,i] = mbar.computeExpectations(h.astype(int))
+        p_of_rxn_coord_min_0[:,i] = p_of_rxn_coord[:,i] - min(p_of_rxn_coord[:,i])
 
-    A_over_RT = -np.log(p_of_Q)
+    A_over_RT = -np.log(p_of_rxn_coord)
     A_over_RT_min_0 = np.zeros(A_over_RT.shape)
-    for i in range(numbins):
-        A_over_RT_min_0[:,i] = A_over_RT[:,i] - min(A_over_RT_min_0[:,i])
+    for i in range(A_over_RT.shape[0]):
+        A_over_RT_min_0[i,:] = A_over_RT[i,:] - np.nanmin(A_over_RT[i,:])
 
-    return A_over_RT, A_over_RT_min_0
-
-
+    return A_over_RT, A_over_RT_min_0, bin_mid
 
 ### NO WAY THIS WORKS ###     
 def make_me_some_spaghetti(protein_name, path_to_contacts, Q_long, numbins, thermo_states,  mbar ):
+    """    
+
+    HOT GARBAGE
     
+    """
+
     cwd = os.getcwd
 
     # Take me to the contacts file, my body is ready
@@ -251,9 +337,10 @@ def make_me_some_spaghetti(protein_name, path_to_contacts, Q_long, numbins, ther
     width = 2./gamma
 
     for i in range(len(contacts)):
-        qi = observables(top, contacts[i], ri_0, width)
-        qtanh = observables.calculate_observable(trajfiles, qtanhsum_obs, saveas="Q.npy")
-        for j in range(len(numbins)):
+        qi = observables.TanhContacts(top, contacts[i], ri_0[i], width)
+        qtanh = observables.calculate_observable(trajfiles, qtanhsum_obs)
+        for j in range(numbins):
+            h = 'which frames are in bin j' 'can move above loop 246 to here'
             Qij, dQij = mbar.computeExpectations(qi*h_expectation[j])/h_expectation[j]
             # Save Qij for all thermo states somehow, need to ask klubes about all of this first
    
@@ -272,19 +359,42 @@ if __name__ == "__main__":
     T_umb = 129.0
 
     atom_pair = np.array([0,57])
-
-    r0 = read_filenames('umbrella_last', path)
     
+    r0 = read_filenames('umbrella_last', path)
+   
     energies, poop = read_data(r0, '/Etot.dat', path)
     
-    u_kn, N_k, r0_interp = umbrella_u_kn_N_k(r0, energies, T_umb, atom_pair, path)
-     
+    u_kn, N_k, r0_interp, r1N_all = umbrella_u_kn_N_k(r0, energies, T_umb, atom_pair, path, num_interp=0)
+    """ 
     mbar = pymbar.MBAR(u_kn, N_k)
     
+    # Turns out I don't need this at all...  
     #Q, Q_long = read_data(r0, '/Q.dat', path)
 
+    A_over_RT, A_over_RT_min_0, bin_mid = free_eng_profile(mbar, r1N_all)
+
+    ##### Plot stuff #####
     
+    plt.figure()
+    for i in range(len(r0_interp) + 1):
+        if i == len(r0_interp):
+            plt.plot(bin_mid,A_over_RT_min_0[i,:],linewidth=5)
+
+        else:
+            plt.plot(bin_mid, A_over_RT_min_0[i,:])  
  
+    centers = [ 'r0 = ' + str(x) + ' nm'  for x in r0_interp ]  
+    
+    centers.append('unbiased')
+ 
+    plt.title('A/RT vs r1N')
+    plt.xlabel('r1N (nm)')
+    plt.ylabel('A/RT')
+    plt.legend(centers, loc=9, prop = {'size':9})
+    
+    plt.savefig("A_over_RT_vs_r1N_min_0.pdf") 
+    """
+
     ##### Test temperature stuff #####
     """
     temps = read_filenames('short_temps', '/home/gsc4/scratch/SH3.constant.temp/test_melt')
@@ -298,7 +408,7 @@ if __name__ == "__main__":
     Cv = calc_Cv(mbar, Etot, T)    
     """
 
-    # Plot stuff
+    ##### Plot stuff #####
     """
 ######## This seems like a super circuitious route to do this... ####
     T_Cv_zip = zip(T,Cv)
