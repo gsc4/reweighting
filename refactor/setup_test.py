d75fd457f13dbedb8481684ff1f0e65cdb14efa4
Campo <gsc4@login1.bird.rice.edu>
2016-06-06 11:45:33 -0500

    tests more things

--- refactor/setup_test.py
+++ refactor/setup_test.py
@@ -6,7 +6,7 @@ from refactored_setup import *
 
 if __name__ == '__main__':
 
-    t1 = time.time()
+#    t1 = time.time()
 
     path = '/home/gsc4/scratch/SH3.constant.temp/test_melt/umbrella_sampling/5-27-2016_sh3_ca_sbm/'
 
@@ -14,8 +14,8 @@ if __name__ == '__main__':
 
     os.chdir(path)
 
-    ##### Constant temp test #####
-
+##### CONSTANT TEMP #####
+    """
     name_format = 'T_{}_{}/{}'
     name_data = [[128.5, 129.0, 129.5], [0,1,2],['Etot.xvg']]
     E_concat, n_frames = read_data_const_temp(name_format, name_data, n_trials=3)
@@ -27,14 +27,12 @@ if __name__ == '__main__':
     temps = [128.5, 129.0, 129.5]
     mbar, T_interp = create_mbar_const_temp(temps, E_concat, n_frames, n_interp=0)
     #Cv = calc_Cv(mbar, E_concat, T_interp)
+    """
+    
+#    print time.time() - t1 
 
-    #dir_name_format = 'T_{}_{}'
-    #dir_name_data = [[129.0], [0,1,2]]
-    #qivsQ, bin_mid, loops = empirical_spaghetti(dir_name_format, dir_name_data, 'SH3', Q_concat)
-    
-    
-    print time.time() - t1 
-    
+#### MBAR SPAGHETTI ####
+    """
     dir_name_format = 'T_{}_{}'
     dir_name_data = [temps, range(3)]
     mbar_qivsQ, bin_mid, loops = mbar_spaghetti(dir_name_format, dir_name_data, 'SH3', Q_concat, len(T_interp), mbar, numbins = 50) 
@@ -46,9 +44,18 @@ if __name__ == '__main__':
         np.save('mbar_QivsQ_' + str(T_interp[i]) + '.npy', mbar_qivsQ[i,:,:])
         
     np.savetxt('temps', T_interp)
+    np.savetxt('loop', loops)
+    np.savetxt('bin_mid', bin_mid)
+    """
 
 #### EMPIRICAL SPAGHETTI    
+
+    #### Qi vs Q ####
     """
+    #dir_name_format = 'T_{}_{}'
+    #dir_name_data = [[129.0], [0,1,2]]
+    #qivsQ, bin_mid, loops = empirical_spaghetti(dir_name_format, dir_name_data, 'SH3', Q_concat)
+    
     max_loop = np.max(loops)
     
     for i in range(len(qivsQ)):
@@ -61,8 +68,10 @@ if __name__ == '__main__':
     plt.savefig('qivsQ.pdf')
     """
 
-#### MBAR SPAGHETTI
-
+#### MBAR SPAGHETTI ####
+    
+    #### Qi vs Q ####
+    """
     max_loop = np.max(loops)    
 
     for i in range(len(T_interp)):
@@ -77,16 +86,45 @@ if __name__ == '__main__':
         plt.ylabel('Percent formation of contact')
         plt.savefig('QivsQ_' + str(T_interp[i]))
         plt.close()       
-     
-    t2 = time.time()
-
-    t_total = t2-t1
-
-    print t_total
-
     """
-    ##### Plot temperature stuff #####
+
+    #### Qi vs r1N ####
+    path = '/home/gsc4/scratch/SH3.constant.temp/test_melt/umbrella_sampling/4-7-2016_end_end_umb/'
     
+    os.chdir(path)
+
+    dir_name_format = '{}'
+    umb_centers = [x.strip('\n') for x in open(str('umbrella_last'), 'r').readlines() ]        
+    dir_name_data = [umb_centers]
+    atom_pair = np.array([0,57])
+    umb_T = 129.
+
+    print ' reading in umb data'
+
+    E_unbias_concat, r1N_concat, k_umb, n_frames = read_data_umb(dir_name_format, dir_name_data, 'SH3', 'r1N.npy', umb_centers, atom_pair)
+    
+    print 'making mbar'    
+
+    mbar, umb_centers_interp = create_mbar_umb(umb_centers, E_unbias_concat, r1N_concat, n_frames, k_umb, umb_T)
+
+    print 'spaghetti math'
+
+    Qi_vs_r1N, bin_mid, loops = mbar_spaghetti(dir_name_format, dir_name_data, 'SH3', r1N_concat, len(umb_centers_interp), mbar)
+
+    max_loop = np.max(loops)
+    
+    #for i in range(len(qivsr1N)
+
+
+
+#    t2 = time.time()
+
+#    t_total = t2-t1
+
+#    print t_total
+
+##### PLOT CONST TEMP  #####
+    """    
     ### argsort to reorder
     order = np.argsort(T_interp)
 
